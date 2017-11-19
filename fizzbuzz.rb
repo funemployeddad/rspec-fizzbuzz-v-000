76c5a233568296dc87a4a8b82c875d8e1131af11
Josh Kramer <funemployeddad@gmail.com>
2017-11-19 21:32:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++
@@ -1,9 +0,0 @@
-def fizzbuzz(value)
-if value/3 == integer 
-  puts "Fizz"
-elsif value/5 == integer
-  puts "Buzz"
-elsif value/3 = integer && value/5 == integer
-  puts "FizzBuzz"
-else puts ""
-end
